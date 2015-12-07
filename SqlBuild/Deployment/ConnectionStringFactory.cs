cc37917166c30a51d486568cf32581e45a8674f3
Jochen Gruen <jochen.gruen@deutschebahn.com>
2015-12-07 11:04:55 +0100

    Added test project for SqlBuild assembly.

---
+++ SqlBuild/Deployment/ConnectionStringFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SqlBuild.Model;
+
+namespace SqlBuild.Deployment
+{
+    public class ConnectionStringFactory
+    {
+        public string CreateConnectionString(SqlConnection connection, SqlLogin login)
+        {
+            
+        }
+    }
+}
